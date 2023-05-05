b4628c4653a6726b5c81b3a693d63fd0a30abf4f
梦境迷离 <dreamylost@outlook.com>
2023-05-05 14:41:46 +0800

    fix

--- postgres/src/main/scala/bitlap/scalikejdbc/binders/AllBinders.scala
+++ postgres/src/main/scala/bitlap/scalikejdbc/binders/AllBinders.scala
@@ -27,4 +27,4 @@ import bitlap.scalikejdbc.core.EnumBinders
  *    梦境迷离
  *  @version 1.0,2023/4/27
  */
-trait AllBinders extends ArrayBinders, EnumBinders
+trait AllBinders extends JsonBinders, ArrayBinders, EnumBinders
