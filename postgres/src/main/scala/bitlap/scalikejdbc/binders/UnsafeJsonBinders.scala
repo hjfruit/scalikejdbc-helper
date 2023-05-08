c2011f32dd71efa4ceede4a3f141797387233dbd
梦境迷离 <dreamylost@outlook.com>
2023-05-08 09:16:58 +0800

    add UnsafeJsonBinders

---
+++ postgres/src/main/scala/bitlap/scalikejdbc/binders/UnsafeJsonBinders.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2023 bitlap
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package bitlap.scalikejdbc.binders
+
+import bitlap.scalikejdbc.internal.*
+import scalikejdbc.*
+
+/** Only support case classes
+ *  @author
+ *    梦境迷离
+ *  @version 1.0,2023/4/8
+ */
+trait UnsafeJsonBinders:
+
+  given unsafeType2Json[T <: Product](using map: T => String): ParameterBinderFactory[T] =
+    DeriveParameterBinder.jsonb[T](map)
+
+  given unsafeJson2Type[T <: Product](using map: String => T): TypeBinder[T] =
+    DeriveTypeBinder.json[T](map)
